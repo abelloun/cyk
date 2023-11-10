2b87fe0d2f6a52ad2e368b1cc5a286fdddebdb57
Colin Blake <colin.blake@universite-paris-saclay.fr>
2023-11-10 18:02:02 +0100

    Semantics

--- main.py
+++ main.py
@@ -81,18 +81,18 @@ GRAMMAR = '''
     mon => DetMasc  {\\P R x. P(x) & possede(moi, x) & R(x)}
     mes => DetMascPlur  {\\P R x. P(x) & possede(moi, x) & R(x)}
     mes => DetFemPlur  {\\P R x. P(x) & possede(moi, x) & R(x)}
-    ses => DetMascPlur  {\\P R x. P(x) & R(x)}
-    ses => DetFemPlur  {\\P R x. P(x) & R(x)}
-    un => DetMasc   {\\P R. exists x. P(x) & R(x)}
-    Un => DetMasc   {\\P R. exists x. P(x) & R(x)}
+    ses => DetMascPlur  {\\P R x y. P(x) & possede(y, x) & R(x)}
+    ses => DetFemPlur  {\\P R x y. P(x) & possede(y, x) & R(x)}
+    un => DetMasc   {\\P R y. exists x. P(x) & R(x)}
+    Un => DetMasc   {\\P R y. exists x. P(x) & R(x)}
 
     # Antépositions
     la => AnteposMasc {\\P. P(\\R x. (féminin(x) & R(x)))}
     le => AnteposMasc {\\P. P(\\R x. (masculin(x) & R(x)))}
     la => AnteposFem {\\P. P(\\R x. (féminin(x) & R(x)))}
     le => AnteposFem {\\P. P(\\R x. (masculin(x) & R(x)))}
-    lui => VbTransSM/VbTransSM {\\P S Q R x z. P(\\T. S(T & (\\u. masculin(x))), Q, R, z)}
-    lui => VbTransSF/VbTransSF {\\P S Q R x z. P(\\T. S(T & (\\u. masculin(x))), Q, R, z)}
+    lui => VbTransSM/VbTransSM {\\P S Q R z y x. P(\\T. S(T & (\\u. cible(y))), Q, \\u. R(z), z, y, x)}
+    lui => VbTransSF/VbTransSF {\\P S Q R z y x. P(\\T. S(T & (\\u. cible(y))), Q, \\u. R(z), z, y, x)}
 
     # Adjectifs
     méchant => AdjMascL {\\P. P & méchant}
@@ -102,8 +102,8 @@ GRAMMAR = '''
     qui => (GrNom[Masc]\\GrNom[Masc])/VbIntransSM {\\P Q R. P(Q, R)}
     qui => (GrNom[FemPlur]\\GrNom[FemPlur])/VbIntransPF {\\P Q R. P(Q, R)}
     qui => (GrNom[MascPlur]\\GrNom[MascPlur])/VbIntransPM {\\P Q R. P(Q, R)}
-    donné => PP[Masc] {\\P x y z. donne(x, y, z) & P(y, z)}
-    mangée => PP[Fem] {\\P x y. mange(x, y) & P(y)}
+    donné => PP[Masc] {\\P x y z. donne(x, y, z) & P(y)}
+    mangée => PP[Fem] {\\P x y z. mange(x, y) & P(y)}
 
     # Superlatif
     très => AdjMascL/AdjMascL {\\P. P}
@@ -116,80 +116,87 @@ GRAMMAR = '''
     Elle => Phrase/VbIntransSF {\\P. P(\\R x. féminin(x) & R(x))}
 
     # Verbes
-    pourchasse => VbTransSF {\\P Q R. Q(\\z. P(pourchasse(z) & (\\s. R(z))))}
-    pourchasse => VbTransSM {\\P Q R. Q(\\z. P(pourchasse(z) & (\\s. R(z))))}
-    attrape => VbTransSF {\\P Q R. Q(\\z. P(attrape(z) & (\\s. R(z))))}
-    attrape => VbTransSM {\\P Q R. Q(\\z. P(attrape(z) & (\\s. R(z))))}
-    mange => VbTransSF {\\P Q R. Q(\\z. P(mange & (\\s. R(z))))}
-    mange => VbTransSM {\\P Q R. Q(\\z. P(mange & (\\s. R(z))))}
-    mange => VbIntransSF {\\P R. P(R & mange)}
-    mange => VbIntransSM {\\P R. P(R & mange)}
-    dorment => VbIntransPM {\\P R. P(R & dort)}
-    dorment => VbIntransPF {\\P R. P(R & dort)}
-    dort => VbIntransSM {\\P R. P(R & dort)}
-    dort => VbIntransSF {\\P R. P(R & dort)}
-    donne => VbTransSM {\\P Q R. Q(\\z. P(donne(z) & (\\s. R(z))))}
-    donne => VbTransSF {\\P Q R. Q(\\z. P(donne(z) & (\\s. R(z))))}
-    souhaite => VbTransSM/VerbeInf {\\P Q R x. P(souhaite(x)) & Q(R, x)}
-    souhaite => VbTransSF/VerbeInf {\\P Q R x. P(souhaite(x)) & Q(R, x)}
-    que => (VbIntransSF\\(VbTransSF/VerbeInf))/Phrase {\\P Q. Q(\\S. S(P))}
-    que => (VbIntransSM\\(VbTransSM/VerbeInf))/Phrase {\\P Q. Q(\\S. S(P))}
+    pourchasse => VbTransSF {\\P Q R x. exists y. Q(\\n. P(pourchasse(n) & (\\s. R(n)), y), x)}
+    pourchasse => VbTransSM {\\P Q R x. exists y. Q(\\n. P(pourchasse(n) & (\\s. R(n)), y), x)}
+    attrape => VbTransSF {\\P Q R x. exists y. Q(\\n. P(attrape(n) & (\\s. R(n)), y), x)}
+    attrape => VbTransSM {\\P Q R x. exists y. Q(\\n. P(attrape(n) & (\\s. R(n)), y), x)}
+    mange => VbTransSF {\\P Q R x. exists y. Q(\\n. P(mange(n) & (\\s. R(n)), y), x)}
+    mange => VbTransSM {\\P Q R x. exists y. Q(\\n. P(mange(n) & (\\s. R(n)), y), x)}
+    mange => VbIntransSF {\\P R x. P(R & mange, x)}
+    mange => VbIntransSM {\\P R x. P(R & mange, x)}
+    dorment => VbIntransPM {\\P R x. P(R & dort, x)}
+    dorment => VbIntransPF {\\P R x. P(R & dort, x)}
+    dort => VbIntransSM {\\P R x. P(R & dort, x)}
+    dort => VbIntransSF {\\P R x. P(R & dort, x)}
+    donne => VbTransSM {\\P Q R x. exists y. exists s. P(\\z. Q(\\u. donne(u, z, y) & R(x), x), s)}
+    donne => VbTransSF {\\P Q R x. exists y. exists s. P(\\z. Q(\\u. donne(u, z, y) & R(x), x), s)}
+    souhaite => VbTransSM/VerbeInf {\\T P Q R x. exists y. exists s. souhaite(T(P, Q, R, x, y, s), x) }
+    souhaite => VbTransSF/VerbeInf {\\T P Q R x. exists y. exists s. souhaite(T(P, Q, R, x, y, s), x) }
+    que => (VbIntransSF\\(VbTransSF/VerbeInf))/Phrase {\\P Q p. Q(\\S. S(P))}
+    que => (VbIntransSM\\(VbTransSM/VerbeInf))/Phrase {\\P Q p. Q(\\U V R x y s. P(R, s, x, y))}
     est => VbIntransSF/AdjFemR {\\P Q R. Q(P(R))}
     est => VbIntransSM/AdjMascL {\\P Q R. Q(P(R))}
     est => VbIntransSF/AdjFemL {\\P Q R. Q(P(R))}
     est => VbIntransSM/AdjMascR {\\P Q R. Q(P(R))}
-    est => VbIntransSF/PP {\\P Q R. P(Q(R))}
-    est => VbIntransSM/PP {\\P Q R. P(Q(R))}
-    donner => VerbeInf {donner}
+    est => VbIntransSF/PP {\\P Q R x. exists y. Q(\\u. P(R, x, u, y))}
+    est => VbIntransSM/PP {\\P Q R x. exists y. Q(\\u. P(R, x, u, y))}
+    donner => VerbeInf {\\P Q R x y s. P(\\z. Q(\\u. donne(u, z, y) & R(x), x), s)}
 
     # Adverbes
+    #paisiblement => AdvM {\\P S R. S(R & \\z. paisiblement(P(\\P. P(z), \\x. true)))}
+    #paisiblement => AdvF {\\P S R. S(R & \\z. paisiblement(P(\\P. P(z), \\x. true)))}
     paisiblement => AdvM {\\P S R. P(S, paisible & R)}
     paisiblement => AdvF {\\P S R. P(S, paisible & R)}
     paisiblement => AdvTM {\\P S R. P(S, paisible & R)}
     paisiblement => AdvTF {\\P S R. P(S, paisible & R)}
 
     # Compléments
-    à => (VbTransSM\\VbTransSM)\\GrNom {\\P Q S T R. S(\\z. P(Q(\\U. T(U(z) & R), transfert, z)))}
-    à => (VbTransSF\\VbTransSF)\\GrNom  {\\P Q S T R. S(\\z. P(Q(\\U. T(U(z) & R), transfert, z)))}
-    à => (PP[Masc]\\PP[Masc])/GrNom {\\P Q R z. P(Q(\\s. R, z))}
-    à => (PP[Fem]\\PP[Fem])/GrNom {\\P Q R z. P(Q(\\s. R, z))}
-    à => (VbTransSM\\AnteposMasc)\\VbTransSM {\\P Q S T R. S(\\z. Q(\\M.M, P(\\U. T(U(z) & R), transfert, z)))}
-    à => (VbTransSF\\AnteposFem)\\VbTransSF {\\P Q S T R. S(\\z. Q(\\M.M, P(\\U. T(U(z) & R), transfert, z)))}
-    avec => AdvF/GrNom {\\P S Q R x z. P(utilise(z), x) & S(Q, R, z) }
-    avec => AdvM/GrNom {\\P S Q R x z. P(utilise(z), x) & S(Q, R, z) }
-    de => (GrNom[Masc]\\GrNom[Masc])/GrNom {\\P Q S x. Q((\\z. P((\\z. S(x)) & appartient(x))), x)}
-    de => (GrNom[Fem]\\GrNom[Fem])/GrNom {\\P Q S x. Q((\\z. P((\\z. S(x)) & appartient(x))), x)}
-    par => (PP[Masc]\\PP[Masc])/GrNom {\\Q R P x y. Q((\\u. R(P,u,y)), x)}
-    par => (PP[Fem]\\PP[Fem])/GrNom {\\Q R P x y. Q((\\u. R(P,u,y)), x)}
+    à => (VbTransSM\\VbTransSM)\\GrNom {\\P Q S T R x y. Q(P, \\M z. S(\\n. M(x), y), T(R), x, y)}
+    à => (VbTransSF\\VbTransSF)\\GrNom  {\\P Q S T R x y. P(Q(T, S, \\z. R(x), y), x)}
+    à => (PP[Masc]\\PP[Masc])/GrNom {\\P Q R x y s. Q(\\u. P(\\n. R(y), s), x, y, s)}
+    à => (PP[Fem]\\PP[Fem])/GrNom {\\P Q R x y s. Q(\\u. P(\\n. R(y), s), x, y, s)}
+    à => (VbTransSM\\AnteposMasc)\\VbTransSM {\\P Q S T R x y s. P(Q(\\O. O), \\M z. S(\\n. M(x), s), T(R), x, s)}
+    à => (VbTransSF\\AnteposFem)\\VbTransSF {\\P Q S T R x y. Q(P(T, S, \\z. R(x), y, x))}
+    avec => AdvF/GrNom {\\P S Q R x z y. S(Q, \\u. P(utilise(z) & \\u. R(z), x, z), z, y) }
+    avec => AdvM/GrNom {\\P S Q R x z y. S(Q, \\u. P(utilise(z) & \\u. R(z), x, z), z, y) }
+    de => (GrNom[Masc]/GrNom)\\GrNom[Masc] {\\P Q R u. exists x. Q(\\z. P(R, x) & appartient(x, u), u)}
+    de => (GrNom[Fem]/GrNom)\\GrNom[Fem] {\\P Q R u. exists x. Q(\\z. P(R, x) & appartient(x, u), u)}
+    par => (PP[Masc]\\PP[Masc])/GrNom {\\P Q R x y s. Q(\\u. P(\\n. R(y), x), x, y, s)}
+    par => (PP[Fem]\\PP[Fem])/GrNom {\\P Q R x y s. Q(\\u. P(\\n. R(y), x), x, y, s)}
 
     # Conjonctions
-    et => (GrNom[MascPlur]/GrNom[Fem])\\GrNom[Masc] {\\P Q R. (P(R) & Q(R))}
-    et => (GrNom[FemPlur]/GrNom[Fem])\\GrNom[Fem] {\\P Q R. (P(R) & Q(R))}
-    et => (GrNom[MascPlur]/GrNom[Masc])\\GrNom[Masc] {\\P Q R. (P(R) & Q(R))}
-    et => (GrNom[MascPlur]/GrNom[Masc])\\GrNom[Fem] {\\P Q R. (P(R) & Q(R))}
+    et => (GrNom[MascPlur]/GrNom[Fem])\\GrNom[Masc] {\\L C R x. exists y. (L(R, x) & C(R, y))}
+    et => (GrNom[FemPlur]/GrNom[Fem])\\GrNom[Fem] {\\L C R x. exists y. (L(R, x) & C(R, y))}
+    et => (GrNom[MascPlur]/GrNom[Masc])\\GrNom[Masc] {\\L C R x. exists y. (L(R, x) & C(R, y))}
+    et => (GrNom[MascPlur]/GrNom[Masc])\\GrNom[Fem] {\\L C R x. exists y. (L(R, x) & C(R, y))}
     #et => (VbTransSM/VbTransSM)\\VbTransSM {\\P Q T N. P(\\S. S, Q(\\U. U, T))}
     #et => (VbTransSF/VbTransSF)\\VbTransSF {\\P Q T N. P(\\S. S, Q(\\U. U, T))}
 
-    et => (VbTransSM/VbTransSM)\\VbTransSM {\\P Q R. (P(R) & Q(R))}
-    et => (VbTransSF/VbTransSF)\\VbTransSF {\\P Q R. (P(R) & Q(R))}
-    et => (VbIntransSF/VbIntransSF)\\VbIntransSF {\\P Q R. (P(R) & Q(R))}
-    et => (VbIntransSM/VbIntransSM)\\VbIntransSM {\\P Q R. (P(R) & Q(R))}
-    et => (AdjFemL/AdjFemL)\\AdjFemL {\\P Q R. (P(R) & Q(R))}
-    et => (AdjFemR/AdjFemR)\\AdjFemR {\\P Q R. (P(R) & Q(R))}
-    et => (AdjMascL/AdjMascL)\\AdjMascL {\\P Q R. (P(R) & Q(R))}
-    et => (AdjMascR/AdjMascR)\\AdjMascR {\\P Q R. (P(R) & Q(R))}
-    et => (Phrase/Phrase)\\Phrase {\\P Q R. (P(R) & Q(R))}
+    et => (VbTransSM/VbTransSM)\\VbTransSM {\\L C P Q R x. exists y. (L(P, Q, R, x, y) & C(P, Q, R, x, y))}
+    #et => (VbTransSM/VbTransSM)\\VbTransSM {\\P Q R x y. Q(\\n. P(pourchasse(n) & (\\s. R(n)), y), x)}             pourchasse(n) & (\\s. R(n))
+
+        # {\\x y. pourchasse(y) & (\\s. R(y))}
+    #et => (VbTransSM/VbTransSM)\\VbTransSM {\\L C P Q R x. exists y. L(P, Q(t), C(\\x y. x, \\s n. s(n), R, x, x), x, y)}
+    et => (VbTransSF/VbTransSF)\\VbTransSF {\\L C P Q R x. exists y. (L(P, Q, R, x, y) & C(P, Q, R, x, y))}
+    et => (VbIntransSF/VbIntransSF)\\VbIntransSF {\\L C P Q x. (L(P, R, x) & C(P, R, x))}
+    et => (VbIntransSM/VbIntransSM)\\VbIntransSM {\\L C P Q x. (L(P, R, x) & C(P, R, x))}
+    et => (AdjFemL/AdjFemL)\\AdjFemL {\\L C P. (L(P) & C(P))}
+    et => (AdjFemR/AdjFemR)\\AdjFemR {\\L C P. (L(P) & C(P))}
+    et => (AdjMascL/AdjMascL)\\AdjMascL {\\L C P. (L(P) & C(P))}
+    et => (AdjMascR/AdjMascR)\\AdjMascR {\\L C P. (L(P) & C(P))}
+    et => (Phrase/Phrase)\\Phrase {\\L C R x. exists y. (C(R, x) & L(R, y))}
+    et => (Phrase/Phrase)\\Phrase {\\L C R x. exists y. (C(R, x) & L(R, y))}
 
     # Phrases interrogatives
     Quel => ((Phrase/Ponct[Interro])/VbIntransSM)/Nom[Masc] {\\P Q R. (Q((\\S. exists x. S(x) & P(x)), R))}
     quel => GrNom[Question]/Nom[Masc] {\\P Q R. (Q((\\S. exists x. S(x) & P(x)), R))}
     Quelle => ((Phrase/Ponct[Interro])/VbIntransSF)/Nom[Fem] {\\P Q R. (Q((\\S. exists x. S(x) & P(x)), R))}
-    quelle => GrNom[Question]/Nom[Fem] {\\P Q R. (Q((\\S. exists x. S(x) & P(x)), R))}
+    quelle => GrNom[Question]/Nom[Fem] {\\P R x. P(x) & R(x)}
     Qui => (Phrase/Ponct[Interro])/VbIntransSM {\\Q R. (Q((\\S. exists x. S(x) & humain(x)), R))}
     Qui => (Phrase/Ponct[Interro])/VbIntransSF {\\Q R. (Q((\\S. exists x. S(x) & humain(x)), R))}
     quoi => GrNom[Question] {\\R x. objet(x) & R(x)}
-    Avec => ((Phrase/Ponct[Interro])/Phrase)/GrNom[Question] {\\P S R x z. P(R, x) & S(\\s. utilise(s, x), z) }
-    A => ((Phrase/Ponct[Interro])/Phrase)/GrNom[Question] {\\P. P}
+    Avec => ((Phrase/Ponct[Interro])/Phrase)/GrNom[Question] {\\P S R x z y. P(R, x) & S(\\u. utilise(u, x), y, z) }
+    A => ((((Phrase/Ponct[Interro])/PP)/(VbIntransSM/PP[Masc]))/GrNom[Masc])/GrNom[Question] {\\Q S V P I. V(\\R x y s. P(\\u. Q(\\n. R(s), s), x, y, s), S, I)}
     ? => Ponct[Interro] {inconnu}
 
 
@@ -209,10 +216,13 @@ TXT_SAMPLE = '''
     Le chat noir dort
     Le méchant chat dort
     Le très méchant chat dort
+    Le chat de la sœur dort
     Le chat de la sœur de mon voisin dort
     Le chat que mon voisin lui donne mange
     Le chat qui dort est noir
     Le chat mange la souris
+    Le chat mange un chat
+    Un chat mange un chat
     Le chat la mange
     Il la mange
     Quel chat mange la souris ?
@@ -224,13 +234,22 @@ TXT_SAMPLE = '''
     Le chat mange la souris avec ses dents
     Le chat la mange avec ses dents
     Avec quoi le chat mange la souris ?
+    Le rat donne un fromage
     Le rat donne un fromage à la souris
+    Un fromage est donné
+    Un fromage est donné à la souris
+    Un fromage est donné par le rat
+    Le chat lui donne la souris
     Un fromage est donné par le rat à la souris
     A quelle souris un fromage est donné par le rat ?
-    Il le donne à la souris
+    Le chat le donne à la souris
     Il le lui donne
+    mon voisin lui donne le chat
     Il souhaite que mon voisin lui donne le chat
+    Il souhaite donner le chat
     Il souhaite donner le chat à mon voisin
+    Le chat de mon voisin pourchasse la souris
+    Le chat pourchasse et attrape la souris
     Le chat de mon voisin pourchasse et attrape la souris
     La souris dort et le chat de mon voisin attrape la souris
     Le chat dort et la souris dort
@@ -258,6 +277,8 @@ Le chat de mon voisin dort
 Le chat de la sœur de mon voisin dort
 '''
 
+word_test = ""
+
 
 def run(txt, grammar):
     """
@@ -280,7 +301,8 @@ def run(txt, grammar):
     # Create a CCGrammar object from the grammar.
     ccg = CCGrammar(grammar)
     cpt = 0
-    print(ccg.show())
+    #
+    #print(ccg.show())
     # Split the input 'txt' by newline characters to
     # process each sentence separately.
     for sentence in (str1.strip() for str1 in txt.split("\n") if str1.strip()):
@@ -293,18 +315,18 @@ def run(txt, grammar):
         parses = CCGCKYParser(ccg, sentence, use_typer=False)
         cpt_n = 0
 
-        if parses and "de" in sentence:
+        if parses and word_test in sentence:
             # Iterate through the parsing results and display them.
             for parse in parses:
-                print(parse.show(sem=True))
-                print(parse.to_nltk_tree().draw())
-                #print(parse.current.expr.show(), parse.current.sem.show())
+                #print(parse.show(sem=True))
+                #print(parse.to_nltk_tree())
+                print(parse.current.expr.show(), parse.current.sem.show(), "\n")
                 cpt_n += 1
-                # break
+                #break
         else:
             # Handle cases where parsing fails.
-            print(f"@@@@@@@@@@@@ FAIL on ::: {sentence} @@@@@@@@@@@@\n")
-
+            #print(f"@@@@@@@@@@@@ FAIL on ::: {sentence} @@@@@@@@@@@@\n")
+            pass
         cpt += cpt_n
 
     # Calculate and display the time taken for parsing.
